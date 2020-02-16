ad78cb20f3ff3646a85c7d13f1d7b917d8929175
Ibrahim <5855449+khan-ibrahim@users.noreply.github.com>
2020-02-16 16:17:08 -0500

    Improved tokenizer regex.

--- cse354sp20_a1_v2.py
+++ cse354sp20_a1_v2.py
@@ -18,7 +18,7 @@ def tokenize(sent):
     # must use regular expressions
 
     tokens = []
-    pattern = r'''[@#0-9A-Za-z\.'\-]+'''
+    pattern = r'''(?:[A-Z]\.)+|\.|[-@#'’1-9A-Za-z]+'''
     ## TODO: fix tokenizer to account for abbreviations ie. S.B.U.
     tokens = re.findall(pattern, sent)
 
